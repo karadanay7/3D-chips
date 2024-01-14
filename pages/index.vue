d359d8c2150b2375a1be7fee1fc767069837b338
Aysegul <karadan.ay7@gmail.com>
2024-01-14 16:14:57 +0300

    3d chips

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="text-red-500">hello</div>
+  <div class="h-screen">
+    <iframe
+      src="https://my.spline.design/chips-6191a94ee5b5248e310b6a7648613d37/"
+      frameborder="0"
+      width="100%"
+      height="100%"
+    ></iframe>
+  </div>
 </template>
 
 <script setup></script>
